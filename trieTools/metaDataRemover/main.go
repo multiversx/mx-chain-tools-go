051f1938d6e139cfea8f5bfe70737d77206fd692
Elrond/ <marius.ciucam@gmail.com>
2022-08-05 15:10:08 +0300

    FEAT: Group tokens by intervals

--- trieTools/metaDataRemover/main.go
+++ trieTools/metaDataRemover/main.go
@@ -6,8 +6,11 @@ import (
 	"github.com/ElrondNetwork/elrond-tools-go/trieTools/trieToolsCommon"
 	"github.com/urfave/cli"
 	"io/ioutil"
+	"math/big"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 )
 
 const (
@@ -56,8 +59,11 @@ func startProcess(c *cli.Context) error {
 	log.Info("starting processing", "pid", os.Getpid())
 
 	tokens, err := readInput(flagsConfig.Tokens)
-	log.Info("read from input", "num of tokens", len(tokens))
+	log.Info("read from input", "file", flagsConfig.Tokens, "num of tokens", len(tokens))
 
+	tokensSorted := sortTokensIDByNonce(tokens)
+
+	_ = groupTokensByIntervals(tokensSorted)
 	return nil
 }
 
@@ -85,4 +91,67 @@ func readInput(tokensFile string) (map[string]struct{}, error) {
 	}
 
 	return tokens, nil
+}
+
+func sortTokensIDByNonce(tokens map[string]struct{}) map[string][]uint64 {
+	ret := make(map[string][]uint64)
+	for token := range tokens {
+		splits := strings.Split(token, "-")
+		tokenID := splits[0] + "-" + splits[1] // ticker-randSequence
+		nonceBI := big.NewInt(0)
+		nonceBI.SetString(splits[2], 16)
+
+		ret[tokenID] = append(ret[tokenID], nonceBI.Uint64())
+	}
+
+	log.Info("found", "num of tokensID", len(ret))
+	for tokenID, nonces := range ret {
+		log.Info("found", "tokenID", tokenID, "num of nonces", len(nonces))
+		sort.SliceStable(nonces, func(i, j int) bool {
+			return nonces[i] < nonces[j]
+		})
+	}
+
+	return ret
+}
+
+type interval struct {
+	start uint64
+	end   uint64
+}
+
+func groupTokensByIntervals(tokens map[string][]uint64) map[string][]*interval {
+	ret := make(map[string][]*interval)
+
+	for token, nonces := range tokens {
+		numNonces := len(nonces)
+		for idx := 0; idx < numNonces; idx++ {
+			nonce := nonces[idx]
+			if idx+1 >= numNonces {
+				ret[token] = append(ret[token], &interval{
+					start: nonce,
+					end:   nonce,
+				})
+				break
+			}
+
+			currInterval := &interval{start: nonce}
+			numConsecutiveNonces := uint64(0)
+			for idx < numNonces-1 {
+				currNonce := nonces[idx]
+				nextNonce := nonces[idx+1]
+				if nextNonce-currNonce > 1 {
+					break
+				}
+
+				numConsecutiveNonces++
+				idx++
+			}
+
+			currInterval.end = currInterval.start + numConsecutiveNonces
+			ret[token] = append(ret[token], currInterval)
+		}
+	}
+
+	return ret
 }
