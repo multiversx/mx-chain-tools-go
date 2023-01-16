6caf56ec4916cfae816c7dbccd1f9db20fce7e95
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2023-01-16 11:29:06 +0200

    change app.Authors

--- trieTools/trieChecker/main.go
+++ trieTools/trieChecker/main.go
@@ -31,8 +31,8 @@ func main() {
 	app.Flags = trieToolsCommon.GetFlags()
 	app.Authors = []cli.Author{
 		{
-			Name:  "The Elrond Team",
-			Email: "contact@elrond.com",
+			Name:  "The MultiversX Team",
+			Email: "contact@multiversx.com",
 		},
 	}
 
